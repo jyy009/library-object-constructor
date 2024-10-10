c6dd2da359273e4fee13e68201ba9b3705fca567
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-10-10 12:02:33 +0200

    Add function to remove book

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const closeButton = document.querySelector(".close-button");
 const bookDialog = document.querySelector(".book-dialog");
 const submitButton = document.querySelector(".submit-button");
 const bookForm = document.querySelector(".book-form");
+// const removeButton = document.createElement("button");
 
 const bookLibrary = [
   {
@@ -25,23 +26,24 @@ function Book(title, author, year, genre) {
   this.author = author;
   this.year = year;
   this.genre = genre;
+  this.read = function() {
+    
+  }
 }
 
 const addBookToLibrary = (title, author, year, genre) => {
   const newBook = new Book(title, author, year, genre);
   bookLibrary.push(newBook);
   console.log(bookLibrary);
-
-
 };
-
 
 const displayBooks = (bookArray) => {
   bookContainer.innerHTML = "";
 
-  bookArray.forEach((book) => {
+  bookArray.forEach((book, index) => {
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
+    bookCard.setAttribute("data-index", index);
 
     const cardTitle = document.createElement("h4");
     cardTitle.classList.add("card-title");
@@ -51,46 +53,72 @@ const displayBooks = (bookArray) => {
     const cardAuthor = document.createElement("p");
     cardAuthor.classList.add("card-author");
     cardAuthor.textContent = book.author;
-    bookCard.appendChild(cardAuthor)
+    bookCard.appendChild(cardAuthor);
 
     const cardYear = document.createElement("p");
     cardYear.classList.add("card-year");
     cardYear.textContent = book.year;
-    bookCard.appendChild(cardYear)
+    bookCard.appendChild(cardYear);
 
     const cardGenre = document.createElement("p");
     cardGenre.classList.add("card-genre");
     cardGenre.textContent = book.genre;
-    bookCard.appendChild(cardGenre)
+    bookCard.appendChild(cardGenre);
 
+    const removeButton = document.createElement("button");
+    removeButton.classList.toggle("remove-button");
+    removeButton.textContent = "Delete";
+    bookCard.appendChild(removeButton);
 
     bookContainer.appendChild(bookCard);
+
+    const readButton = document.createElement("button");
+    readButton.classList.toggle("read-button");
+    readButton.textContent = "Mark Read"
+    bookCard.appendChild(readButton)
   });
 };
 
-showButton.addEventListener("click", () => {
-  bookDialog.showModal()
+
+bookContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-button")) {
+    const card = e.target.closest(".book-card")
+    const index = card.dataset.index
+    removeBook(index)
+  }
 })
+
+bookContainer.addEventListener("click", (e) => {
+  
+})
+
+const removeBook = (index) => {
+  bookLibrary.splice(index, 1)
+  displayBooks(bookLibrary)
+}
+
+showButton.addEventListener("click", () => {
+  bookDialog.showModal();
+});
 
 closeButton.addEventListener("click", () => {
-  bookDialog.close()
-  bookForm.reset()
-
-})
+  bookDialog.close();
+  bookForm.reset();
+});
 
 bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const title = bookForm.querySelector('input[name="book-title"]').value
-  const author = bookForm.querySelector('input[name="book-author"]').value
-  const year = bookForm.querySelector('input[name="book-year"]').value
-  const genre = bookForm.querySelector('input[name="book-genre"]').value
+  const title = bookForm.querySelector('input[name="book-title"]').value;
+  const author = bookForm.querySelector('input[name="book-author"]').value;
+  const year = bookForm.querySelector('input[name="book-year"]').value;
+  const genre = bookForm.querySelector('input[name="book-genre"]').value;
 
-  addBookToLibrary(title, author, year, genre)
+  addBookToLibrary(title, author, year, genre);
   displayBooks(bookLibrary);
 
-  bookForm.reset()
-  bookDialog.close()
-})
+  bookForm.reset();
+  bookDialog.close();
+});
 
 displayBooks(bookLibrary);
